493a0c4e0913ae9da4eebb91a5678d77b4ff02dc
Aaron Anglin <aaron.anglin101@gmail.com>
2023-06-14 17:26:10 -0500

    added modal

--- app/page.js
+++ app/page.js
@@ -1,86 +1,103 @@
 "use client";
-
+import { useState } from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "react-chartjs-2";
 
-import ExpenseCategoryItems from "@/components/ExpenseCategoryItems";
 import { currencyFormatter } from "@/util/format";
+import ExpenseCategoryItems from "@/components/ExpenseCategoryItems";
+import Modal from "@/components/Modal";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
+const DummyData = [
+  {
+    id: 1,
+    title: "Food",
+    color: "red",
+    total: 200,
+  },
+  {
+    id: 2,
+    title: "Transportation",
+    color: "blue",
+    total: 500,
+  },
+  {
+    id: 3,
+    title: "Housing",
+    color: "green",
+    total: 1400,
+  },
+  {
+    id: 4,
+    title: "Clothing",
+    color: "yellow",
+    total: 300,
+  },
+];
+
 export default function Home() {
-  const DummyData = [
-    {
-      id: 1,
-      title: "Food",
-      color: "red",
-      total: 200,
-    },
-    {
-      id: 2,
-      title: "Transportation",
-      color: "blue",
-      total: 500,
-    },
-    {
-      id: 3,
-      title: "Housing",
-      color: "green",
-      total: 1400,
-    },
-    {
-      id: 4,
-      title: "Clothing",
-      color: "yellow",
-      total: 300,
-    },
-  ];
+  const [showModal, setShowModal] = useState(true);
+
   return (
-    <main className="container max-w-2xl px-6 mx-auto">
-      <section className="py-3">
-        <small className="text-md">My Balance</small>
-        <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
-      </section>
-      <section className="flex items-center gap-2 py-3">
-        <button className="btn btn-primary">+ Expenses</button>
-        <button className="btn btn-primary">+ Income</button>
-      </section>
-      {/* Expense Section */}
-      <section className="py-6">
-        <h1 className="text-2xl">My Expenses</h1>
-        <div className="flex flex-col gap-4 mt-6">
-          {DummyData.map((expense) => {
-            return (
-              <ExpenseCategoryItems
-                key={expense.id}
-                color={expense.color}
-                title={expense.title}
-                total={expense.total}
-              />
-            );
-          })}
-        </div>
-      </section>
-      {/* Chart Section */}
-      <section className="py-6">
-        <h3 className="text-2xl">Stats</h3>
-        <div className="w-1/2 mx-auto py-2">
-          <Doughnut
-            data={{
-              labels: DummyData.map((expense) => expense.title),
-              datasets: [
-                {
-                  label: "Expenses",
-                  data: DummyData.map((expense) => expense.total),
-                  backgroundColor: DummyData.map((expense) => expense.color),
-                  borderColor: DummyData.map((expense) => expense.color),
-                  borderWidth: 1,
-                },
-              ],
+    <>
+      {/* Modal */}
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <h1>Hello World</h1>
+      </Modal>
+      <main className="container max-w-2xl px-6 mx-auto">
+        <section className="py-3">
+          <small className="text-md">My Balance</small>
+          <h2 className="text-4xl font-bold">{currencyFormatter(100000)}</h2>
+        </section>
+        <section className="flex items-center gap-2 py-3">
+          <button
+            onClick={() => {
+              setShowModal(true);
             }}
-          />
-        </div>
-      </section>
-    </main>
+            className="btn btn-primary"
+          >
+            + Expenses
+          </button>
+          <button className="btn btn-primary">+ Income</button>
+        </section>
+        {/* Expense Section */}
+        <section className="py-6">
+          <h1 className="text-2xl">My Expenses</h1>
+          <div className="flex flex-col gap-4 mt-6">
+            {DummyData.map((expense) => {
+              return (
+                <ExpenseCategoryItems
+                  key={expense.id}
+                  color={expense.color}
+                  title={expense.title}
+                  total={expense.total}
+                />
+              );
+            })}
+          </div>
+        </section>
+        {/* Chart Section */}
+        <section className="py-6">
+          <h3 className="text-2xl">Stats</h3>
+          <div className="w-1/2 mx-auto py-2">
+            <Doughnut
+              data={{
+                labels: DummyData.map((expense) => expense.title),
+                datasets: [
+                  {
+                    label: "Expenses",
+                    data: DummyData.map((expense) => expense.total),
+                    backgroundColor: DummyData.map((expense) => expense.color),
+                    borderColor: DummyData.map((expense) => expense.color),
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+            />
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
---
+++ components/Modal.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Modal({ showModal, setShowModal, children }) {
+  return (
+    <div
+      style={{
+        transform: showModal ? "translateX(0%)" : "translateX(-200%)",
+      }}
+      className="absolute top-0 left-0 w-full h-full  z-10 transition-all duration-500"
+    >
+      <div className="container mx-auto max-w-2xl h-[90vh] rounded-3xl bg-gray-700 py-6 px-4 mt-3">
+        <button
+          onClick={() => {
+            setShowModal(false);
+          }}
+          className="w-10 h-10 mb-4 font-bold rounded-full bg-gray-500"
+        >
+          X
+        </button>
+        {children}
+      </div>
+    </div>
+  );
+}
