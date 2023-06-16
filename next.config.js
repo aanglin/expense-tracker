4e2a9ced68c8613f581238eff3a26a62897e4c10
Aaron Anglin <aaron.anglin101@gmail.com>
2023-06-16 11:23:59 -0500

    turned into pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,45 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { GenerateSW } = require("workbox-webpack-plugin");
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer, dev }) => {
+    if (!isServer && !dev) {
+      config.plugins.push(
+        new GenerateSW({
+          swDest: "static/service-worker.js",
+          exclude: [
+            /\.map$/,
+            /_app\.js$/,
+            /_document\.js$/,
+            /_error\.js$/,
+            /server\.js$/,
+            /chunk\-pages\-manifest\.json$/,
+          ],
+          runtimeCaching: [
+            {
+              urlPattern: /^https?.*/,
+              handler: "NetworkFirst",
+              options: {
+                cacheName: "offlineCache",
+                expiration: {
+                  maxEntries: 200,
+                },
+              },
+            },
+          ],
+        })
+      );
+    }
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
+
+// module.exports = nextConfig
